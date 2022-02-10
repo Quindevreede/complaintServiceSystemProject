599358056f75ec6ec960d1807e5a3264371568eb
Quindevreede <quindevreede@hotmail.com>
2022-02-10 13:35:15 +0100

    Complaint Service System Program ALL ENDPOINTS WORK CLEANED CODE

--- src/test/java/nl/quin/complaintservicesystem/service/ReceiptUploadServiceTest.java
+++ src/test/java/nl/quin/complaintservicesystem/service/ReceiptUploadServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
